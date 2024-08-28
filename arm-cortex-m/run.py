7a885932ba646be99a3a2fb12db1565c35d3fe99
Caleb Helbling <chelbling@draper.com>
2024-08-28 16:23:42 -0400

    Converted tabs to spaces

--- arm-cortex-m/run.py
+++ arm-cortex-m/run.py
@@ -3,24 +3,24 @@ import angr
 proj = angr.Project("test.o")
 
 def run_fun(name, expected_primask):
-	print("RUNNING FUNCTION {}".format(name))
-	fun_addr = proj.loader.find_symbol(name).rebased_addr
-	state = proj.factory.call_state(fun_addr, prototype="void f()")
+    print("RUNNING FUNCTION {}".format(name))
+    fun_addr = proj.loader.find_symbol(name).rebased_addr
+    state = proj.factory.call_state(fun_addr, prototype="void f()")
 
-	print("primask before", state.regs.primask)
+    print("primask before", state.regs.primask)
 
-	simgr = proj.factory.simgr(state)
-	while len(simgr.active) > 0:
-		simgr.step()
+    simgr = proj.factory.simgr(state)
+    while len(simgr.active) > 0:
+        simgr.step()
 
-	for deadended_state in simgr.deadended:
-		print("primask after", deadended_state.regs.primask)
-		if deadended_state.regs.primask.concrete_value != expected_primask:
-			print("TEST FAILED!")
-		else:
-			print("TEST PASSED")
+    for deadended_state in simgr.deadended:
+        print("primask after", deadended_state.regs.primask)
+        if deadended_state.regs.primask.concrete_value != expected_primask:
+            print("TEST FAILED!")
+        else:
+            print("TEST PASSED")
 
-	print("\n\n")
+    print("\n\n")
 
 run_fun("test", 1)
 run_fun("test2", 1)
